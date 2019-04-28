43f3e33b2bda1ef1b14ac178c5fa5581ef0dbacd
Pilow <pierre.laupretre@gmail.com>
2019-04-28 13:13:20 +0800

    [main] Update test script

--- main.py
+++ main.py
@@ -3,30 +3,39 @@ import subprocess
 import os
 import time
 import hashlib
+import json
 
 GENERATOR_V1 = 'java -jar ../generator-v1/leekscript.jar '
 GENERATOR_V2 = '../generator/build/leek-wars-generator -q '
 
-start = time.time()
-proc = subprocess.Popen(GENERATOR_V1 + 'scenario/fight_v1.json', shell=True, stdout=subprocess.PIPE)
-result1, err = proc.communicate()
-end = time.time()
+def main():
+	result1, time1, hash1 = run(GENERATOR_V1 + 'scenario/fight_v1.json')
+	result2, time2, hash2 = run(GENERATOR_V2 + 'scenario/fight_v1.json')
+	parsed1 = json.loads(result1)
+	parsed2 = json.loads(result2)
 
-start2 = time.time()
-proc = subprocess.Popen(GENERATOR_V2 + 'scenario/fight_v1.json', shell=True, stdout=subprocess.PIPE)
-result2, err = proc.communicate()
-end2 = time.time()
+	if (hash1 == hash2):
+		print("Result : " + result1)
+	else:
+		print("Result 1 : " + result1)
+		print("Result 2 : " + result2)
 
-print("Result 1 : " + result1)
-print("Result 2 : " + result2)
+	print("V1: " + format_time(time1) + " " + hash1)
+	print("V2: " + format_time(time2) + " " + hash2)
+	print('OK' if hash1 == hash2 else 'FAIL')
 
-h1 = hashlib.new('sha1')
-h1.update(result1)
-hash1 = h1.hexdigest()
+def run(command):
+	start = time.time()
+	proc = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE)
+	result, err = proc.communicate()
+	end = time.time()
+	h = hashlib.new('sha1')
+	h.update(result)
+	hash = h.hexdigest()
+	return (result, end - start, hash)
 
-h2 = hashlib.new('sha1')
-h2.update(result2)
-hash2 = h2.hexdigest()
+def format_time(time):
+	return str(round(time * 10000) / 10) + 'ms'
 
-print("Time v1", end - start, hash1)
-print("Time v2", end2 - start2, hash2)
+if __name__ == '__main__':
+    main()
