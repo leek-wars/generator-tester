7ed57d0d973d4f7f54c8d7b29d1937cad9787979
Pilow <pierre.laupretre@gmail.com>
2019-06-09 16:07:02 +0200

    [scenario] Add quine scenario

--- main.py
+++ main.py
@@ -34,6 +34,7 @@ def run_all():
 		"004_logs.json",
 		"005_max_turns.json",
 		"006_medium.json",
+		"007_quine.json",
 	]
 	print(color.BOLD + str(len(scenarios)) + " scenarios to run..." + color.END)
 	success = 0
